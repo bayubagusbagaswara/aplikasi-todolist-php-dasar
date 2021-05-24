c3f0aa363e93b5e441d731cde3a267d9f93991c8
bayubagusbagaswara <bagaszwara12@gmail.com>
2021-05-24 15:10:23 +0700

    logic untuk menampilkan todolist

--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -5,4 +5,13 @@
  */
 function showTodoList()
 {
+    // ambil model datanya
+    global $todoList;
+
+    // kita tampilkan list dari arraynya, pake perulangan foreach
+    echo "TODOLIST" . PHP_EOL;
+
+    foreach ($todoList as $number => $value) {
+        echo "$number. $value" . PHP_EOL;
+    }
 }
