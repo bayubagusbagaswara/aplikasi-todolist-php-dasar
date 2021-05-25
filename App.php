f4ed6300c1f7ed5f33d71dcfea2081b3200a7f71
bayubagusbagaswara <bagaszwara12@gmail.com>
2021-05-25 16:31:12 +0700

    sukses test aplikasi todolist secara keseluruhan

--- App.php
+++ App.php
@@ -1,15 +1,19 @@
 <?php
-
+// tambahkan __DIR__ untuk mengganti absolute path/directory, karena kita menggunakan relatif directory
 // require data modelnya
-require_once "./model/TodoList.php";
+require_once __DIR__ . "/Model/TodoList.php";
 // require business logic
-require_once "./BusinessLogic/ShowTodoList.php";
-require_once "./BusinessLogic/AddTodoList.php";
-require_once "./BusinessLogic/RemoveTodoList.php";
+require_once __DIR__ . "/BusinessLogic/ShowTodoList.php";
+require_once __DIR__ . "/BusinessLogic/AddTodoList.php";
+require_once __DIR__ . "/BusinessLogic/RemoveTodoList.php";
 // require view
-require_once "./View/ViewShowTodoList.php";
-require_once "./View/ViewAddTodoList.php";
-require_once "./View/ViewRemoveTodoList.php";
-
+require_once __DIR__ . "/View/ViewShowTodoList.php";
+require_once __DIR__ . "/View/ViewAddTodoList.php";
+require_once __DIR__ . "/View/ViewRemoveTodoList.php";
+// require helper
+require_once __DIR__ . "/Helper/Input.php";
 
 echo 'Aplikasi Todolist' . PHP_EOL;
+
+// Langsung display layar pertama yakni menampilkan todolist
+viewShowTodoList();
